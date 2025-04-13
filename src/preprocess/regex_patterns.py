27b5e9b871c13bbe776de6c20fb045165df37d53
Taiki0404 <whitepenguin0404@gmail.com>
2025-04-13 14:57:37 +0000

    add: RegexPatternsクラスを追加

---
+++ src/preprocess/regex_patterns.py
@@ -0,0 +1,7 @@
+class RegexPatterns:
+    URL = r"https?://(?:www\.)?[^\s/$.?#].[^\s]*"
+    PARENTHESES_WITH_TEXT = r"\(.*?\)"
+    MENTIONS = r"@\w+"
+    TAGS = r"#\w+"
+    PAGE_NOTATION = r"\d+/\d+"
+    SYMBOLS = r"[!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~�©®™]"
