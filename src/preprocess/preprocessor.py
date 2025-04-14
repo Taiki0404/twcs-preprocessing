e161dd671299181dda2d1ccd02ae497f0941792e
taiki <whitepenguin0404@gmail.com>
2025-04-14 05:20:59 +0000

    change: TextReplacerをインスタンス変数として追加

--- src/preprocess/preprocessor.py
+++ src/preprocess/preprocessor.py
@@ -1,8 +1,10 @@
 from preprocess.cleaner import TextCleaner
 from preprocess.normalizer import TextNormalizer
+from preprocess.replacer import TextReplacer
 
 
 class Preprocessor:
     def __init__(self):
         self.cleaner = TextCleaner()
+        self.replacer = TextReplacer()
         self.normalizer = TextNormalizer()
