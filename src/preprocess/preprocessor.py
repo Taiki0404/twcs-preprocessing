f69b15af20ef609ae4c3cbcd5426c626615e2b80
Taiki0404 <whitepenguin0404@gmail.com>
2025-04-14 13:31:32 +0000

    remove: TextReplacerクラスを削除

--- src/preprocess/preprocessor.py
+++ src/preprocess/preprocessor.py
@@ -1,10 +1,8 @@
 from preprocess.cleaner import TextCleaner
 from preprocess.normalizer import TextNormalizer
-from preprocess.replacer import TextReplacer
 
 
 class Preprocessor:
     def __init__(self):
         self.cleaner = TextCleaner()
-        self.replacer = TextReplacer()
         self.normalizer = TextNormalizer()
