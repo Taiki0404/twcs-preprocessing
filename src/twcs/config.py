e83641137c9e875d3dfa298d1c1dc48319759de4
Taiki0404 <whitepenguin0404@gmail.com>
2025-04-16 17:24:56 +0000

    add: twcsの処理に関する設定を記述するためのファイルを追加

---
+++ src/twcs/config.py
@@ -0,0 +1,7 @@
+columns = {
+    "to_use_in_text_table": ["tweet_id", "text"],
+    "to_use_in_pair_table": ["tweet_id", "response_tweet_id", "in_response_to_tweet_id"],
+    "for_meta_table": ["tweet_id", "author_id", "inbound", "created_at"],
+    "for_text_table": ["tweet_id", "processed_text"],
+    "for_pair_table": ["utterance", "response", "dialog_id"],
+}
