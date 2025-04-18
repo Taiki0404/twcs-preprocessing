a83866e6b1351220b14b1ea1c79f3eaa2ccb3b5e
Taiki0404 <whitepenguin0404@gmail.com>
2025-04-18 15:36:21 +0000

    delete: 使わない列設定を削除

--- src/twcs/config.py
+++ src/twcs/config.py
@@ -1,5 +1,4 @@
 columns = {
-    "to_use_in_text_table": ["tweet_id", "text"],
     "to_use_in_pair_table": ["tweet_id", "response_tweet_id", "in_response_to_tweet_id"],
     "for_meta_table": ["tweet_id", "author_id", "inbound", "created_at"],
     "for_text_table": ["tweet_id", "processed_text"],
