25f000d38c0c79380e8e7eb2a75890518676a663
Taiki0404 <whitepenguin0404@gmail.com>
2025-04-18 14:13:47 +0000

    feat: すべての対話をtweet_idのリストとして抽出するロジックを追加

--- src/twcs/twcs.py
+++ src/twcs/twcs.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 from .config import columns
+from .extractor import TwcsExtractor
 
 
 class TWCS:
@@ -12,6 +13,8 @@ class TWCS:
 
         self.twcs = pd.read_csv(twcs_path)
         self.columns = columns
+
+        self.extractor = TwcsExtractor()
 
     def retrieve_metadata(self) -> pd.DataFrame:
         return self.twcs[self.columns["for_meta_table"]]
@@ -23,6 +26,17 @@ class TWCS:
         ...
         return
 
-    def extract_dialog_pairs(self) -> pd.DataFrame:
-        ...
-        return
+    def extract_dialog_branches(self) -> list[list[int]]:
+        dialog_roots = self.twcs[
+            pd.isnull(self.twcs["in_response_to_tweet_id"]) & self.twcs["inbound"]
+        ]
+        dialog_start_ids = dialog_roots["tweet_id"].tolist()
+
+        dialog_branches = self.extractor.extract_all_dialog_branching(
+            dialog_start_ids, self.to_dict()
+        )
+
+        return dialog_branches
+
+    def to_dict(self) -> dict[int, dict]:
+        return self.twcs.set_index("tweet_id").to_dict("index")
