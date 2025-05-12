01f86b13bd9deb8467a04fa76a7463def2f7028f
taiki <whitepenguin0404@gmail.com>
2025-05-12 04:05:42 +0000

    change: dialogIDの指定をファイル経由に変更

--- generate_plain_texts.py
+++ generate_plain_texts.py
@@ -12,6 +12,12 @@ META_CSV_PATH = "./output/metadata.csv"
 PAIR_CSV_PATH = "./output/pair.csv"
 
 
+def read_file_of_dialog_ids(path: str) -> list[int]:
+    with open(path, "r") as f:
+        lines = f.readlines()
+    return [int(line.strip()) for line in lines]
+
+
 def to_txt(path: str, texts: list[str]) -> None:
     with open(path, "w") as f:
         for text in texts:
@@ -21,7 +27,10 @@ def to_txt(path: str, texts: list[str]) -> None:
 def argparse_args():
     parser = argparse.ArgumentParser(description="Generate plain text from dialog data.")
     parser.add_argument(
-        "--dialog_id", type=int, required=True, help="The ID of the dialog to generate text for."
+        "--input-path",
+        type=str,
+        default=None,
+        help="Path to a file containing a list of dialog IDs.",
     )
     return parser.parse_args()
 
@@ -40,10 +49,12 @@ if __name__ == "__main__":
     table_handler = TableHandler(df_text, df_meta, df_pair)
     ptext_generator = PlainTextGenerator(table_handler, rules)
 
-    plain_text = ptext_generator.generate_plain_texts(args.dialog_id)
+    for dialog_id in read_file_of_dialog_ids(args.input_path):
+        plain_text = ptext_generator.generate_plain_texts(dialog_id)
 
-    if plain_text is None:
-        print("No valid dialog found.")
-    else:
-        path = f"{OUTPUT_DIR}/dialog_{args.dialog_id}.txt"
-        to_txt(path, plain_text)
+        if plain_text is None:
+            print(f"Dialog ID {dialog_id} not found in the data.")
+            # TODO: logging
+        else:
+            path = f"{OUTPUT_DIR}/dialog_{dialog_id}.txt"
+            to_txt(path, plain_text)
