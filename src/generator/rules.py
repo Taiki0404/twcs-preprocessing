f2335f5f402e80479d9609b93a739ea7663f1216
Taiki0404 <whitepenguin0404@gmail.com>
2025-05-14 02:56:43 +0000

    change: Ruleを扱うRuleSetクラスを追加

--- src/generator/rules.py
+++ src/generator/rules.py
@@ -9,6 +9,17 @@ class Rule(metaclass=ABCMeta):
         pass
 
 
+class RuleSet:
+    def __init__(self, rules: list[Rule] = []):
+        self.rules = rules
+
+    def apply_all(self, dialog: Dialog) -> bool:
+        return all(rule.apply(dialog) for rule in self.rules)
+
+    def add(self, rule: Rule):
+        self.rules.append(rule)
+
+
 class NumAuthorsRule(Rule):
     def __init__(self, num_authors: int):
         self.num_authors = num_authors
