f4df41679463cc7850b7f9b414f2db3aca51ef49
Taiki0404 <whitepenguin0404@gmail.com>
2025-05-14 07:41:57 +0000

    add: 対話の長さに関するルールを追加

--- src/twcs/sampling/rules.py
+++ src/twcs/sampling/rules.py
@@ -26,3 +26,13 @@ class NumAuthorsRule(Rule):
 
     def apply(self, dialog: Dialog) -> bool:
         return len(set(dialog.authors)) == self.num_authors
+
+
+class SequenceLengthRule(Rule):
+    def __init__(self, min_length: int, max_length: int):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def apply(self, dialog: Dialog) -> bool:
+        joined_texts = dialog.join_continuous_texts_by_same_author()
+        return self.min_length <= len(joined_texts) <= self.max_length
